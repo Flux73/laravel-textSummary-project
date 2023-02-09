11f067f4d2c685af694f3026fc613b9bd40c1121
Flux73 <salahmoumni5@gmail.com>
2023-02-09 12:36:34 +0100

    Home Page UI

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,15 +7,17 @@
     @vite('resources/css/app.css')
     <title>Document</title>
 </head>
-<body class="flex items-center justify-center h-screen text-lg  font-medium" >
-    <div class="bg-emerald-50 shadow-xl p-10 rounded-lg max-w-4xl">
-        <h1 class=" text-3xl font-black text-emerald-900  text-center mb-12">You have the option to use the tool in two ways :</h1>
-        <div class="container flex flex-col gap-4 mb-7">
-            <div class="flex gap-2">
-                <div class="container flex items-center justify-center rounded-full w-10 h-10 bg-fuchsia-200 text-fuchsia-900" >
-                    <span class="font-bold text-xl">1</span>
-                </div>
-                <p>
+<body class="flex items-center justify-center h-screen text-lg font-medium px-2 bg-slate-200" >
+    <div class="bg-white shadow-xl rounded-lg w-4/6 overflow-hidden flex">
+        <div class="container bg-center bg-cover w-9/12" style="background-image: url({{ url('/img/e.jpg') }})"></div>
+        <div class="container p-10">
+            <h1 class=" text-3xl font-black text-emerald-900 text-center mb-12">Choose from two options to use the tool. :</h1>
+            <div class="container flex flex-col gap-4 mb-7">
+                <div class="flex gap-2">
+                    <div class="container flex items-center justify-center rounded-full w-10 h-10 bg-emerald-100 text-emerald-900" >
+                        <span class="font-bold text-xl">1</span>
+                    </div>
+                    <p>
                     Use the text summarizing tool without creating an account. Note that access to other app features requires an account.
                     
                 </p>
@@ -28,12 +30,13 @@
                     Create a complete free account to access all features besides the tool.
                 </p>
             </div>
-        </div>
-        <div class="container flex flex-col gap-4 font-bold">
-            <a class="border-2 border-fuchsia-600 text-fuchsia-600 hover:bg-fuchsia-200 transition-all text-center rounded-md py-5" href="{{ route('home') }}">Continue Without An Account</a>
+            </div>
+            <div class="container flex flex-col gap-4 font-bold">
+            <a class="border-2 border-emerald-600 text-emerald-600 hover:bg-emerald-100 transition-all text-center rounded-md py-5" href="{{ route('home') }}">Continue Without An Account</a>
             <a class="bg-blue-600 text-gray-50 hover:bg-blue-700 transition-all text-center rounded-md py-5" href="{{ route('register') }}">Create My Account</a>
             <p class="text-center font-medium text-gray-500">Already have one! simply log in.</p>
             <a class="border-2 border-blue-600 hover:bg-blue-200 transition-all text-blue-600 text-center rounded-md py-5" href="{{ route('login') }}">Log In</a>
+            </div>
         </div>
     </div>
 </body>
